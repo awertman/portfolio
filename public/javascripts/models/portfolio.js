b3fedf91274d2887c2e6d4908e5c0cfed832858e
Aaron Wertman <aaron.wertman@gmail.com>
2014-06-05 13:28:26 -0700

    styling for portfolio with new images

--- public/javascripts/models/portfolio.js
+++ public/javascripts/models/portfolio.js
@@ -7,29 +7,28 @@ app.factory('portfolioContent', function() {
 
 
 PORTFOLIODATA = [ {
-              title: "Codefusion",
+              title: "Codefusion.io",
               imageLink: "images/ember.png",
               description: "Tool designed to helpers programming instructors share code with students. This app allows instructors to share their code live, while students can take notes on a web platform.",
               technology: "Javascript app leveraging Node/Ember.js and a Firebase database.",
               githubLink:"https://github.com/awertman/",
-              liveLink: "http://codefusion.io/#/classrooms/demo",
-              websiteLink:""},
+              liveLink: "http://codefusion.io/#/classrooms/demo"},
+
+           // {
+           //    title: "Brand-img",
+           //    imageLink: "images/rails.png",
+           //    description: "Small businesses resources that helps users understand how customers are talking about their brand. Instagram data is analyzed by comparing correlations between different hash tags used in conjunction with the brand.",
+           //    technology: "Ruby on Rails app using Postgres database and Instagram API.",
+           //    githubLink:"https://github.com/awertman/",
+           //    websiteLink:""},
 
            {
-              title: "Brand-img",
-              imageLink: "images/rails.png",
-              description: "Small businesses resources that helps users understand how customers are talking about their brand. Instagram data is analyzed by comparing correlations between different hash tags used in conjunction with the brand.",
-              technology: "Ruby on Rails app using Postgres database and Instagram API.",
-              githubLink:"https://github.com/awertman/",
-              websiteLink:""},
-
-           {
-              title: "eCommerce Data",
-              imageLink: "images/angular.png",
-              description: "Interactive chart designed to help small business understand how eCommerce is changing in their marketplace.",
-              technology: "Javascript app with Angular.js framework and Highcharts for visual interface.",
-              githubLink:"https://github.com/awertman/",
-              websiteLink:""},
+              title: "Destination Finder",
+              imageLink: "images/node.png",
+              description: "Listing of top rated destinations for 2014 that allows for filtering by keyword, and view changes by grid or list view.",
+              technology: "Javascript app on Node with Angular.js framework and MongoDB.",
+              githubLink:"https://github.com/awertman/destinationFinder",
+              liveLink:"http://destinationfinder.herokuapp.com/"},
 
            {
               title: "vanilla Worm",
@@ -37,14 +36,21 @@ PORTFOLIODATA = [ {
               description: "Game of Worm written in vanilla Javascript using HTML5 canvas.",
               technology: "pure Javascript and HTML5 canvas.",
               githubLink:"https://github.com/awertman/vanillaWorm",
-              websiteLink:""},
+              liveLink:"http://awertman.github.io/vanillaWorm/"},
+
+          // {
+          //     title: "eCommerce Data",
+          //     imageLink: "images/angular.png",
+          //     description: "Interactive chart designed to help small business understand how eCommerce is changing in their marketplace.",
+          //     technology: "Javascript app with Angular.js framework and Highcharts for visual interface.",
+          //     githubLink:"https://github.com/awertman/",
+          //     websiteLink:""},
 
            {
               title: "This website..",
               imageLink: "images/angular.png",
-              description: "My personal website written Javascript with Angular.js framework.",
+              description: "My personal website written in Javascript with Angular.js framework.",
               technology: "Angular.js.",
-              githubLink:"https://github.com/awertman/",
-              websiteLink:""}
+              githubLink:"https://github.com/awertman/portfolio_website"}
 
   ]
