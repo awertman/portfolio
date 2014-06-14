1ec0154f5bcf04a134b7851ce42f657f39890f16
Aaron Wertman <aaron.wertman@gmail.com>
2014-06-14 10:01:01 -0700

    added ng click method to scroll to top of screen

--- public/javascripts/maincontroller.js
+++ public/javascripts/maincontroller.js
@@ -2,6 +2,10 @@ var app = angular.module("profile",['headerFilter'] )
 
 mainController = function($scope,portfolioContent) {
   $scope.portfolioContent = portfolioContent
+  $scope.updateViewHeight = function() {
+    window.scrollTo(0,0)
+  }
+
   $scope.layout = 'about'
 
 }
