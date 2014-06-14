9d42677924a6dbe9b5b6c18f5d583b4547d9d3b9
Aaron Wertman <aaron.wertman@gmail.com>
2014-06-14 10:47:04 -0700

    added conditional to scroll click to ensure href

--- public/javascripts/maincontroller.js
+++ public/javascripts/maincontroller.js
@@ -3,7 +3,9 @@ var app = angular.module("profile",['headerFilter'] )
 mainController = function($scope,portfolioContent) {
   $scope.portfolioContent = portfolioContent
   $scope.updateViewHeight = function() {
-    window.scrollTo(0,0)
+    if(event.target.href) {
+      window.scrollTo(0,0)
+    }
   }
 
   $scope.layout = 'about'
